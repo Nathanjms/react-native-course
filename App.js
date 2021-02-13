cd2cbf225bf3d9698d48587c7c499836b5e5443e
Nathan James <nathanjms@outlook.com>
2021-02-13 19:35:26 +0000

    update

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { Text, View } from "react-native";
+// import { ProgressView } from "@react-native-community/progress-view";
+import { 
+  Text,
+  View, 
+  ActivityIndicator, 
+  Button,
+  Alert,
+  Dimensions,
+  Platform
+} from "react-native";
+
+const { height, width } = Dimensions.get("window");
 
 export default function App() {
+  const onButtonPress = () => {
+    Alert.alert(`${new Date().toLocaleTimeString()} button pressed`)
+  }
   return (
     <View style={{ padding:50 }}>
-      <Text>Hello World</Text>
-      <Text>Test</Text>
-
+      {/* <ProgressView
+        progressTintColor="orange"
+        trackTintColor="blue"
+        progress={0.7}
+      /> */}
+      <ActivityIndicator size="large" color="#61DBFB" />
+      <Button title="click me" onPress={onButtonPress}/>
+      <Text>OS: {Platform.OS}</Text>
+      <Text>Height: {height}</Text>
+      <Text>Width: {width}</Text>
     </View>
   )
 }
