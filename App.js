f4f025c43796222eb951e36c605184fdf5f0d6f0
Nathan James <nathanjms@outlook.com>
2021-02-14 14:45:42 +0000

    Background color change on click

--- App.js
+++ App.js
@@ -1,29 +1,36 @@
-import React from "react";
-import { StyleSheet, Image, View, Dimensions } from "react-native";
+import React, {useState} from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
 
 import imgRicko from "./assets/ricko.jpg"
 import imgRickoTwo from "./assets/ricko2.jpg"
 
 
 export default function App() {
-  
+  const [backgroundColor, setbackgroundColor] = useState("blue");
   return (
-    <View style={styles.page}>
-      <Image style={styles.image} source={imgRicko}/>
-      <Image style={styles.image} source={imgRickoTwo}/>
+    <View style={[styles.container, { backgroundColor }]}>
+      <Text style={styles.button}
+        onPress={ () => setbackgroundColor("green")}>green</Text>
+      <Text style={styles.button}
+      onPress={ () => setbackgroundColor("red")}>red</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  page: {
+  container: {
     flex: 1,
+    display: "flex",
     justifyContent: "center",
     alignItems: "center"
   },
-  image: {
-    flex: 1,
-    borderRadius: 50,
+  button: {
+    fontSize: 30,
+    margin: 10,
+    padding:10,
+    textAlign: "center",
+    borderRadius: 10,
+    borderWidth: 3,
     margin:10,
     width: Dimensions.get("window").width-10
   }
